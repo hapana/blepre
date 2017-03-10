b717566fd414e3408db7b5b37379f4180a73cca6
harry <harry.panayiotou@secretsales.com>
2017-03-10 15:50:31 +0000

    Add src dir

---
+++ src/github.com/hapana/blepre/blepre.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "fmt"
+
+    "github.com/shimomo/ibeacon-scanner"
+)
+
+func main() {
+    ibeacon.Scan(func(uuid string, major string, minor string, rssi int) {
+        fmt.Println(uuid)
+        fmt.Println(major)
+        fmt.Println(minor)
+        fmt.Println(rssi)
+    }, 1000)
+}
