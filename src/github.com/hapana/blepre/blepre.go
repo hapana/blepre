46540da35e30fb551b52a222a2ecf5a3d5abc644
harry <harry.panayiotou@secretsales.com>
2017-03-13 14:07:04 +0000

    Add yaml file reading and strength ranges

--- src/github.com/hapana/blepre/blepre.go
+++ src/github.com/hapana/blepre/blepre.go
@@ -2,15 +2,84 @@ package main
 
 import (
     "fmt"
+    "io/ioutil"
+    "path/filepath"
 
+    "github.com/smallfish/simpleyaml"
+    //"github.com/jamesmillerio/go-ifttt-maker"
     "github.com/shimomo/ibeacon-scanner"
 )
 
 func main() {
+
+    configPath, _ := filepath.Abs("./config.yml")
+    config, err := ioutil.ReadFile(configPath)
+    if err != nil {
+        panic(err)
+    }
+
+    yaml, err := simpleyaml.NewYaml(config)
+    if err != nil {
+        panic(err)
+    }
+
+    room, err := yaml.Get("room").String()
+    if err != nil {
+        panic(err)
+    }
+    fmt.Printf("Room: %#v\n", room)
+
+    makerkey, err := yaml.Get("maker").Get("key").String()
+    if err != nil {
+        panic(err)
+    }
+    fmt.Printf("Maker Key: %#v\n", makerkey)
+
+    onevent, err := yaml.Get("rooms").Get(room).Get("on-event").String()
+    if err != nil {
+        panic(err)
+    }
+    fmt.Printf("On-Event: %#v\n", onevent)
+
+    offevent, err := yaml.Get("rooms").Get(room).Get("off-event").String()
+    if err != nil {
+        panic(err)
+    }
+    fmt.Printf("Off-Event: %#v\n", offevent)
+
+    lowsignal, err := yaml.Get("rooms").Get(room).Get("low-signal").Int()
+    if err != nil {
+        panic(err)
+    }
+    fmt.Printf("Low-Signal: %#v\n", lowsignal)
+
+    highsignal, err := yaml.Get("rooms").Get(room).Get("high-signal").Int()
+    if err != nil {
+        panic(err)
+    }
+    fmt.Printf("High-Signal: %#v\n", highsignal)
+
+    whitelist, err := yaml.Get("whitelist").Array()
+    if err != nil {
+        panic(err)
+    }
+    fmt.Printf("Value: %#v\n", whitelist)
+
+    //maker := new(GoIFTTTMaker.MakerChannel)
+
     ibeacon.Scan(func(uuid string, major string, minor string, rssi int) {
-        fmt.Println(uuid)
-        fmt.Println(major)
-        fmt.Println(minor)
-        fmt.Println(rssi)
+        for i := 0; i < len(whitelist); i += 1 {
+            v := whitelist[i]
+            if v == uuid {
+                if lowsignal < rssi && rssi < highsignal {
+                    fmt.Println(uuid)
+                    fmt.Println(major)
+                    fmt.Println(minor)
+                    fmt.Println(rssi)
+                }
+            }else{
+                fmt.Println("UUID not allowed: %#u\n", uuid)
+            }
+        }
     }, 1000)
 }
